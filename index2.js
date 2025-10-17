aef8f86c21d6d3ed93b4632fd18f49641dcfcf95
IvanR96 <irios96@hotmail.com>
2025-10-16 22:27:24 -0700

    morgan package used

--- index2.js
+++ index2.js
@@ -3,12 +3,13 @@ import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
 app.use(morgan("combined"));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get("/", (req,res) =>{
+  res.send("Hello!");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(port, ()=>{
+  console.log(`Server running on port ${port}`);
 });
