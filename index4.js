6af5582927eda13cb915e04ca4bc40505c85d8a7
IvanR96 <irios96@hotmail.com>
2025-10-18 00:04:56 -0700

    bandname generator completed

--- index4.js
+++ index4.js
@@ -7,25 +7,27 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 let bandName = "";
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bandNameGenerator);
 
+
 function bandNameGenerator(req, res, next){
   console.log(req.body);
-  bandName = req.body["street"] + req.body["pet"];
+  bandName = req.body.street + req.body.pet;
+  console.log(bandName);
   next();
+
 };
 
-
-app.post("/submit", (req, res) =>{
-  console.log(req.body);
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`);
-});
-
-app.get("/", (req, res) => {
+app.get("/", (req, res) =>{
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.post("/submit", (req, res) =>{
+  res.send(`<h1> Your band name is:</h1> <h2>${bandName}! Cool band name :)</h2>`)
+});
+
+app.listen(port, () =>{
+  console.log(`Server running on port ${port}...`);
 });
